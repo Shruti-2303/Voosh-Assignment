7bc56fcd17e1df3c4083ddc852716fa0fed2ccf6
Shruti-2303 <sharmas23032001@gmail.com>
2024-05-16 20:00:59 +0530

    feat: Add database connection setup

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const connection = require("./config/database");
+require("dotenv").config();
 const app = express();
+
+connection();
+
 app.use(express.json());
 app.listen(() => console.log(`Server started on port 3000`));
